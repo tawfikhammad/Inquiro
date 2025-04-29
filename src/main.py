1199f11e5e0c73660f5abefd0e69c8364e0e3c6a
tawfikhammad <tawfikhammad@icloud.com>
2025-04-29 23:40:50 +0300

    now main call WelcomeUI

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
-from controllers import WelcomeController
+from views.welcome_view import WelcomeUI
+from config import app_settings, AppSettings
 
 def main():
-    welcome_controller = WelcomeController()
+    settings = app_settings()
+    welcome_ui = WelcomeUI(settings)
+    welcome_ui.show()
 
 if __name__ == "__main__":
     main()
