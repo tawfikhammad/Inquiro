7bbeb0400f3ddbf3c504f3d85d0c059b3b38fb16
tawfikhammad <tawfikhammad@icloud.com>
2025-05-06 02:23:54 +0300

    database enums

--- backend/utils/enums/database_enums.py
+++ backend/utils/enums/database_enums.py
@@ -4,5 +4,5 @@ class DatabaseEnums(Enum):
 
     PROJECT_COLLECTION_NAME = "projects"
     CHUNK_COLLECTION_NAME = "chunks"
-    ASSET_COLLECTION_NAME = "assets"
+    PAPER_COLLECTION_NAME = "papers"
     SUMMARY_COLLECTION_NAME = "summaries"
