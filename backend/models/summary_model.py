1baeb0a11f552f3baebf99eb7e82ad3ad598c234
tawfikhammad <tawfikhammad@icloud.com>
2025-05-07 02:42:58 +0300

    add update_summary func

--- backend/models/summary_model.py
+++ backend/models/summary_model.py
@@ -23,26 +23,38 @@ class SummaryModel(BaseModel):
         summary.id = res.inserted_id
         return summary
     
+    async def update_summary(self, summary: Summary):
+        result = await self.collection.update_one(
+            {"_id": summary.id},
+            {"$set": summary.dict(by_alias=True, exclude={"id"}, exclude_unset=True)}
+        )
+        return result.modified_count > 0
+    
+    
     async def get_summary_by_project(self, summary_project_id: str, summary_id: str):
         record = await self.collection.find_one(
             {"summary_project_id": ObjectId(summary_project_id) if isinstance(summary_project_id, str) else summary_project_id,
-            "summary_id": summary_id})
+            "_id": ObjectId(summary_id) if isinstance(summary_id, str) else summary_id})
         if record is None:
             return None
         return Summary(**record)
     
-    async def get_summaries_by_project(self, summary_project_id: str, summary_type: str):
-        records = await self.collection.find(
-            {"summary_project_id": ObjectId(summary_project_id) if isinstance(summary_project_id, str) else summary_project_id, 
-            "summary_type": summary_type
-             }).to_list(length=None)
-        if records is None:
-            return None
+    
+    async def get_summaries_by_project(self, summary_project_id: str, summary_type: str = None):
+        query = {"summary_project_id": ObjectId(summary_project_id) if isinstance(summary_project_id, str) else summary_project_id}
+        
+        if summary_type:
+            query["summary_type"] = summary_type
+            
+        records = await self.collection.find(query).to_list(length=None)
+        if not records:
+            return []
         summaries = [Summary(**record) for record in records]
         return summaries
+    
     
     async def delete_summary_by_project(self, project_summary_id: str, summary_id: str):
         result = await self.collection.delete_one(
             {"summary_project_id": ObjectId(project_summary_id) if isinstance(project_summary_id, str) else project_summary_id,
-             "summary_id": summary_id})
+             "_id": ObjectId(summary_id) if isinstance(summary_id, str) else summary_id})
         return result.deleted_count > 0
