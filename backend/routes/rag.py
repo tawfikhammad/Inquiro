76d554d8fee0d7b70cba9468fa01ae7213a9eb15
tawfikhammad <tawfikhammad@icloud.com>
2025-05-30 07:10:54 +0300

    create rag router

---
+++ backend/routes/rag.py
@@ -0,0 +1,171 @@
+from fastapi import APIRouter, Request, Body, status
+from fastapi.responses import JSONResponse
+from backend.controllers import RAGController
+from backend.models import ProjectModel, ChunkModel
+from .schema.rag import PushRequest, SearchRequest
+from utils.enums import ResponseSignals
+import logging
+logger = logging.getLogger('uvicorn.error')
+
+rag_router = APIRouter()
+rag_controller = RAGController()
+
+
+@rag_router.post("/index/push/")
+async def index_project(request: Request, project_id: str, push_request: PushRequest):
+
+    project_model = await ProjectModel.get_instance(db_client=request.app.db_client)
+    chunk_model = await ChunkModel.get_instance(db_client=request.app.db_client)
+
+    project = await project_model.get_project_by_id(project_id=project_id)
+
+    if not project:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": ResponseSignals.PROJECT_NOT_FOUND_ERROR.value
+            }
+        )
+    
+    rag_controller = RAGController(
+        vectordb_client=request.app.vectordb_client,
+        generation_client=request.app.generation_client,
+        embedding_client=request.app.embedding_client,
+    )
+
+    has_records = True
+    page_no = 1
+    inserted_items_count = 0
+    idx = 0
+
+    while has_records:
+        page_chunks = await chunk_model.get_poject_chunks(project_id=project.id, page_no=page_no)
+        if len(page_chunks):
+            page_no += 1
+        
+        if not page_chunks or len(page_chunks) == 0:
+            has_records = False
+            break
+
+        chunks_ids =  list(range(idx, idx + len(page_chunks)))
+        idx += len(page_chunks)
+        
+        is_inserted = rag_controller.index_into_vector_db(
+            project=project,
+            chunks=page_chunks,
+            do_reset=push_request.do_reset,
+            chunks_ids=chunks_ids
+        )
+
+        if not is_inserted:
+            return JSONResponse(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content={
+                    "signal": ResponseSignals.INSERT_INTO_VECTORDB_ERROR.value
+                }
+            )
+        
+        inserted_items_count += len(page_chunks)
+        
+    return JSONResponse(
+        content={
+            "signal": ResponseSignals.INSERT_INTO_VECTORDB_SUCCESS.value,
+            "inserted_items_count": inserted_items_count
+        }
+    )
+
+@rag_router.get("/index/info")
+async def get_project_index_info(request: Request, project_id: str):
+    
+    project_model = await ProjectModel.get_instance(db_client=request.app.db_client)
+    project = await project_model.get_project_by_id(project_id=project_id)
+
+    rag_controller = RAGController(
+        vectordb_client=request.app.vectordb_client,
+        generation_client=request.app.generation_client,
+        embedding_client=request.app.embedding_client,
+    )
+
+    collection_info = rag_controller.get_vector_db_collection_info(project=project)
+
+    return JSONResponse(
+        content={
+            "signal": ResponseSignals.VECTORDB_COLLECTION_RETRIEVED.value,
+            "collection_info": collection_info
+        }
+    )
+
+@rag_router.post("/index/search")
+async def search_index(request: Request, project_id: str, search_request: SearchRequest):
+    
+    project_model = await ProjectModel.get_instance(db_client=request.app.db_client)
+    project = await project_model.get_project_by_id(
+        project_id=project_id
+    )
+
+    rag_controller = RAGController(
+        vectordb_client=request.app.vectordb_client,
+        generation_client=request.app.generation_client,
+        embedding_client=request.app.embedding_client,
+        template_parser=request.app.template_parser,
+    )
+
+    results = rag_controller.search_vector_db_collection(
+        project=project, text=search_request.text, limit=search_request.limit
+    )
+
+    if not results:
+        return JSONResponse(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content={
+                    "signal": ResponseSignals.VECTORDB_SEARCH_ERROR.value
+                }
+            )
+    
+    return JSONResponse(
+        content={
+            "signal": ResponseSignals.VECTORDB_SEARCH_SUCCESS.value,
+            "results": [ result.dict()  for result in results ]
+        }
+    )
+
+@rag_router.post("/index/answer")
+async def answer_rag(request: Request, project_id: str, search_request: SearchRequest):
+    
+    project_model = await ProjectModel.create_instance(
+        db_client=request.app.db_client
+    )
+
+    project = await project_model.get_project_or_create_one(
+        project_id=project_id
+    )
+
+    rag_controller = RAGController(
+        vectordb_client=request.app.vectordb_client,
+        generation_client=request.app.generation_client,
+        embedding_client=request.app.embedding_client,
+        template_parser=request.app.template_parser,
+    )
+
+    answer, full_prompt, chat_history = rag_controller.answer_rag_question(
+        project=project,
+        query=search_request.text,
+        limit=search_request.limit,
+    )
+
+    if not answer:
+        return JSONResponse(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content={
+                    "signal": ResponseSignals.RAG_ANSWER_ERROR.value
+                }
+        )
+    
+    return JSONResponse(
+        content={
+            "signal": ResponseSignals.RAG_ANSWER_SUCCESS.value,
+            "answer": answer,
+            "full_prompt": full_prompt,
+            "chat_history": chat_history
+        }
+    )
