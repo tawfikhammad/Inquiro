a609f5989ac412f16f2feb62fcdd091c1bfc3383
tawfikhammad <tawfikhammad@icloud.com>
2025-06-13 22:39:05 +0300

    refactor environment configuration and app settings structure

--- backend/config/app_settings.py
+++ backend/config/app_settings.py
@@ -10,9 +10,30 @@ class AppSettings(BaseSettings):
 
     MONGO_URL: str
     MONGO_DB: str
+
+    GENERATION_BACKEND : str
+    EMBEDDING_BACKEND : str
+    SUMMARY_BACKEND : str
     
+    OPENAI_API_KEY: str
+    OPENAI_API_URL: str
+    COHERE_API_KEY: str
+    GEMINI_API_KEY : str
+
+    GENERATION_MODEL_ID : str
+    EMBEDDING_MODEL_ID : str
+    EMBEDDING_MODEL_SIZE : int
+    SUMMARY_MODEL_ID : str
+
+    INPUT_DAFAULT_MAX_CHARACTERS : int
+    GENERATION_DAFAULT_MAX_TOKENS : int
+    GENERATION_DAFAULT_TEMPERATURE : float
+
+    VECTOR_DB_BACKEND : str
+    VECTOR_DB_PATH : str
+    VECTOR_DB_DISTANCE_METHOD :str
     class Config:
-        env_file = "src/.env"  
+        env_file = "backend/.env"  
 
 def app_settings() -> AppSettings:
     return AppSettings()
