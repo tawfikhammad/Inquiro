0c7a4057adca75bb2c83fbd8966d29f24756cc7e
tawfikhammad <tawfikhammad@icloud.com>
2025-06-13 22:41:54 +0300

    correct db_path retrieval

--- backend/AI/VectorDB/VDBFactory.py
+++ backend/AI/VectorDB/VDBFactory.py
@@ -7,7 +7,7 @@ class VDBProviderFactory:
         self.config = config
 
     def create(self, provider: str):
-        db_path = PathUtils.get_vdb_path(provider)
+        db_path = PathUtils().get_vdb_path(db_name= self.config.VECTOR_DB_PATH)
         if provider == VectorDBType.QDRANT.value:
             qdrant_provider = QdrantProvider(db_path, self.config.VECTOR_DB_DISTANCE_METHOD)
             return qdrant_provider
