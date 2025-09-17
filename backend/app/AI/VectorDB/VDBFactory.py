6106c81b5d544c126195ce2f4646145584ed02af
tawfikhammad <tawfikhammad@icloud.com>
2025-09-17 21:51:27 +0300

    refactor VDBProviderFactory and VectorDBInterface for improved async support

--- backend/app/AI/VectorDB/VDBFactory.py
+++ backend/app/AI/VectorDB/VDBFactory.py
@@ -1,17 +1,19 @@
 from .providers import QdrantProvider
-from utils import PathUtils
 from .VDBEnums import VectorDBType
+from utils.enums import ResponseSignals
 
 class VDBProviderFactory:
     def __init__(self, config):
         self.config = config
 
     def create(self, provider: str):
-        db_path = PathUtils().get_vdb_path(db_name= self.config.VECTOR_DB_PATH)
         if provider == VectorDBType.QDRANT.value:
-            qdrant_provider = QdrantProvider(db_path, self.config.VECTOR_DB_DISTANCE_METHOD)
-            return qdrant_provider
+            return QdrantProvider(
+                host= self.config.VECTOR_DB_HOST,
+                port= self.config.VECTOR_DB_PORT,
+                grpc_port= self.config.VECTOR_DB_GRPC_PORT,
+                distance_metric= self.config.VECTOR_DB_DISTANCE_METHOD,
+            )
         else:
-            raise ValueError(f"Unsupported provider: {provider}")
-        
+            raise ValueError(ResponseSignals.VDB_UNSUPPORTED_PROVIDER.value)
         
