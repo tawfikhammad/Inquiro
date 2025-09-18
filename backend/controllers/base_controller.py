44c557e2f75ea7ce8ae07063cd35cd736c72f6c4
tawfikhammad <tawfikhammad@icloud.com>
2025-09-19 00:56:11 +0300

    path_utils to base controller

--- backend/controllers/base_controller.py
+++ backend/controllers/base_controller.py
@@ -1,6 +1,8 @@
 from utils import get_settings, AppSettings
+from utils import PathUtils
 
 class BaseController:
 
     def __init__(self):
         self.app_settings = get_settings()
+        self.path_utils = PathUtils()
