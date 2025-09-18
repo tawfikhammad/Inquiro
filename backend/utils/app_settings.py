226daea1b334002ce93cd90a238e7f213e53f979
tawfikhammad <tawfikhammad@icloud.com>
2025-09-19 00:53:52 +0300

    improve formatting in app settings

--- backend/utils/app_settings.py
+++ backend/utils/app_settings.py
@@ -5,30 +5,35 @@ class AppSettings(BaseSettings):
     APP_VERSION: str
 
     ALLOWED_FILE_TYPES: list[str]
-    MAX_FILE_SIZE : int
-    CHUNK_SIZE : int
+    MAX_FILE_SIZE: int
+    CHUNK_SIZE: int
 
     MONGO_URL: str
     MONGO_DB: str
 
-    GENERATION_BACKEND : str
-    EMBEDDING_BACKEND : str
-    SUMMARY_BACKEND : str
+    GENERATION_BACKEND: str
+    EMBEDDING_BACKEND: str
+    SUMMARY_BACKEND: str
     
-    GEMINI_API_KEY : str
+    GEMINI_API_KEY: str
 
-    GENERATION_MODEL_ID : str
-    EMBEDDING_MODEL_ID : str
-    EMBEDDING_SIZE : int
-    SUMMARY_MODEL_ID : str
+    GENERATION_MODEL_ID: str
+    EMBEDDING_MODEL_ID: str
+    EMBEDDING_SIZE: int
+    SUMMARY_MODEL_ID: str
 
-    DEFAULT_MAX_INPUT_CHARACTERS : int
-    GENERATION_DEFAULT_MAX_TOKENS : int
-    GENERATION_DEFAULT_TEMPERATURE : float
+    DEFAULT_MAX_INPUT_CHARACTERS: int
+    GENERATION_DEFAULT_MAX_TOKENS: int
+    GENERATION_DEFAULT_TEMPERATURE: float
 
-    VECTOR_DB_BACKEND : str
-    VECTOR_DB_PATH : str
-    VECTOR_DB_DISTANCE_METHOD :str
+    VECTOR_DB_BACKEND: str
+    VECTOR_DB_HOST: str
+    VECTOR_DB_PORT: int
+    VECTOR_DB_GRPC_PORT: int
+    VECTOR_DB_DISTANCE_METHOD: str
+
+    LANG: str
+    DEFAULT_LANG: str
     class Config:
         env_file = "backend/.env"  
 
