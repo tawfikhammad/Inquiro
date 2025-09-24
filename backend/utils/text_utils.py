1478afa6ab05230204a215251e809c5608250dfb
tawfikhammad <tawfikhammad@icloud.com>
2025-09-24 17:25:59 +0300

    refactor Cleaner class to improve regex patterns and remove unused methods

--- backend/utils/text_utils.py
+++ backend/utils/text_utils.py
@@ -5,8 +5,9 @@ class Cleaner:
     def __init__(self):
         self.HEADER_PATTERN = re.compile(r'^(#{1,4})\s+(.+)$')   # Matches markdown headers (#, ##, ###, ####)
         self.CLEAN_TITLE_PATTERN = re.compile(r'^\d+\.?\s*')     
-        self.NON_WORD_PATTERN = re.compile(r'[^\w\s-]')
+        self.NON_WORD_PATTERN = re.compile(r'[^\w\s.-]')
         self.SPACE_DASH_PATTERN = re.compile(r'[-\s]+')
+        self.REMOVE_HEADER_PATTERN = re.compile(r'^(bibliography|acknowledgment|acknowledgments)$', re.I)
 
     def filename_cleaner(self, filename: str) -> str:
         cleaned = self.SPACE_DASH_PATTERN.sub('_', filename)
@@ -15,11 +16,5 @@ class Cleaner:
         return cleaned.strip('_').lower()
     
     def text_cleaner(self, text: str) -> str:
-        text = re.sub(r"(?i)\\*(references|bibliography)\\*[\s\S]*", "", text)
-        return text
-    
-    def title_to_id(self, title: str) -> str:
-        clean_title = self.CLEAN_TITLE_PATTERN.sub("", title)
-        section_id = self.NON_WORD_PATTERN.sub("", clean_title.lower())
-        section_id = self.SPACE_DASH_PATTERN.sub("_", section_id)
-        return section_id.strip("_")
+        text = re.sub(r'(?im)^#{1,4}\s*(references)\b.*[\s\S]*', '',text)
+        return text.strip()
